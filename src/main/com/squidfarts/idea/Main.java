dd8275493dff5d6af2d13842d22ec3e846657c38
Micheal Brockus <42789598+squidfarts@users.noreply.github.com>
2019-07-23 16:23:20 -0700

    Add files via upload

---
+++ src/main/com/squidfarts/idea/Main.java
@@ -0,0 +1,26 @@
+/***********************************************************************************
+ *                                                                                 *
+ * NAME: main.java                                                                 *
+ *                                                                                 *
+ * AUTHOR: Michael Brockus.                                                        *
+ *                                                                                 *
+ * CONTACT: <mailto:michael@squidfarts.com>                                        *
+ *                                                                                 *
+ * NOTICES:                                                                        *
+ *                                                                                 *
+ * License: MIT                                                                    *
+ *                                                                                 *
+ ***********************************************************************************/
+package com.squidfarts.idea;
+
+
+/*
+ * This demonstrates a simple hello world program where the message
+ * 'Hello, Java' is desplayed to the standerd console.
+ *
+ */
+class Main {
+    public static void main(String [] args) {
+        System.out.println("Hello, Java.");
+    }
+}// end of function main
